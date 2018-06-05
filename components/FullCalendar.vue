f9fbf48876285baa97ac8f2acd568594710fefa0
Neil <vault13tech@gmail.com>
2018-06-05 16:18:07 +0800

    增加自訂按鈕功能及事件

--- components/FullCalendar.vue
+++ components/FullCalendar.vue
@@ -118,7 +118,7 @@
                     dayClick(...args){
                         self.$emit('day-click', ...args)
 					},
-					
+
                     select(start, end, jsEvent, view, resource) {
                         self.$emit('event-created', {
                             start,
@@ -128,7 +128,7 @@
                             resource
                         })
 					},
-					
+
 					...defaultProps,
 					eventMouseover: (event, jsEvent, view) => {
 						// 滑鼠移到行程上的效果
@@ -143,16 +143,12 @@
 					viewRender: (view, element) => {
 						// 六日紅背景
 						this.$(element).find('td.fc-day.fc-sat').css('background-color', '#FFDDDD99')
-						this.$(element).find('td.fc-day.fc-sun').css('background-color', '#FFDDDD99')
-						// 點擊標題事件
+                        this.$(element).find('td.fc-day.fc-sun').css('background-color', '#FFDDDD99')
+                        // 標題style
                         this.$('div#calendar').find('div.fc-center').css({
                             'cursor': 'pointer',
-                            'user-select': 'none'
+                            'user-select': 'none',
                         })
-						this.$('div#calendar').find('div.fc-center').off('click')
-                        this.$('div#calendar').find('div.fc-center').on('click', () => {
-							self.$emit('title-click')
-						})
 						self.$emit('view-render', view, element)
                     },
                 }
@@ -162,13 +158,13 @@
         mounted() {
             const cal = $(this.$el)
 			self = this
-			//點擊中間區塊(不一定是title)
-			// this.$(cal).ready(() => {
-            //     this.$(cal).find('div.fc-center').on('click', (event) => {
-            //         console.log('center clicked')
-            //     })
-			// })
-			
+			//點擊標題事件
+            this.$(cal).ready(() => {
+                this.$(cal).find('div.fc-center').on('click', () => {
+                    self.$emit('title-click')
+                })
+            })
+
             this.$on('remove-event', (event) => {
                 if(event && event.hasOwnProperty('id')){
                     $(this.$el).fullCalendar('removeEvents', event.id);
@@ -208,6 +204,79 @@
             fireMethod(...options) {
                 return $(this.$el).fullCalendar(...options)
             },
+            /**
+             * 組成右上角的按鈕群
+             * * button格式: { text, buttonName, badge, class, visible, enable }
+             * * event: 統一呼叫 customButtonClick (button = { name: 'buttonName' })
+             */
+            setTopRightButtons(buttons = []) {
+                $(this.$el).find('div.fc-right').empty()
+                self = this
+                buttons.map(val => {
+                    const elementPanel = document.createElement('span')
+                    this.$(elementPanel).css('position', 'relative')
+                    this.$(elementPanel).attr('name', val.buttonName + 'Panel')
+
+                    const button = document.createElement('button')
+                    this.$(button).addClass(val.class ? val.class : 'el-button el-button--danger el-button-mini')
+                    this.$(button).text(val.text)
+                    if (val.hasOwnProperty('enable')) {
+                        if (val.enable === false) {
+                            this.$(button).attr('disabled', 'disabled')
+                            this.$(button).addClass('is-disabled')
+                        }
+                    }
+                    if (val.hasOwnProperty('visible')) {
+                        this.$(button).css('display', val.visible === true ? 'inline-block' : 'none')
+                    }
+                    this.$(button).attr('name', val.buttonName)
+                    this.$(button).on('click', () => {
+                        self.$emit('custom-button-click', { name: val.buttonName })
+                    })
+                    elementPanel.appendChild(button)
+
+                    const badge = document.createElement('sup')
+                    const displayBadge = val.badge && val.badge.toString().length > 0;
+                    this.$(badge).addClass('el-badge__content is-fixed')
+                    this.$(badge).text(val.badge)
+                    this.$(badge).css({
+                        'display': displayBadge ? 'inline-block' : 'none',
+                        'z-index': 10,
+                        'right': '15px'
+                    })
+                    elementPanel.appendChild(badge)
+
+                    $(this.$el).find('div.fc-right')[0].appendChild(elementPanel)
+                })
+            },
+            /**
+             * 更新自訂按鈕的文字、通知氣泡等…
+             */
+            updateButton(name, val = {}) {
+                const button = $(this.$el).find('button[name="' + name + '"]')
+                if (button.length > 0) {
+                    if (val.text) {
+                        $(button).text(val.text)
+                    }
+                    if (val.badge) {
+                        const displayBadge = val.badge && val.badge.toString().length > 0;
+                        $(button[0].nextSibling).text(val.badge)
+                        $(button[0].nextSibling).css(displayBadge ? 'inline-block' : 'none')
+                    }
+                    if (val.hasOwnProperty('enable')) {
+                        if (val.enable === true) {
+                            this.$(button).removeAttr('disabled')
+                            this.$(button).removeClass('is-disabled')
+                        } else {
+                            this.$(button).attr('disabled', 'disabled')
+                            this.$(button).addClass('is-disabled')
+                        }
+                    }
+                    if (val.hasOwnProperty('visible')) {
+                        this.$(button).css('display', val.visible === true ? 'inline-block' : 'none')
+                    }
+                }
+            }
         },
 
         watch: {
